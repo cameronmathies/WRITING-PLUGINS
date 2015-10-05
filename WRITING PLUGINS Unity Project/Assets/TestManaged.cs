46e5bd5559b85db16e0863e78c72a120e348ac78
cameronmathies <cmathies@IT202.mr.preble.com>
2015-10-05 13:21:10 -0400

    DLL

---
+++ WRITING PLUGINS Unity Project/Assets/TestManaged.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManaged : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RandomNumber.MyClass obj = new RandomNumber.MyClass ();
+		print ("managed Random Number: " + obj.GetRandom());
+	}
+	
+
+}
